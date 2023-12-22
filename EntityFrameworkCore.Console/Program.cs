5082e7d7e573225b3b8d10c510b6906e2015fc48
rkeenan <ross.keenan@protonmail.comm>
2023-12-21 19:17:48 -0600

    Added methods for finding a single record.

--- EntityFrameworkCore.Console/Program.cs
+++ EntityFrameworkCore.Console/Program.cs
@@ -1,3 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using EntityFrameworkCore.Data;
+using Microsoft.EntityFrameworkCore;
 
-Console.WriteLine("Hello, World!");
+// Create instance of context
+using var context = new FootballLeagueDbContext();
+
+
+//GetAllTeams();
+
+// Getting the first one in the list
+var teamOne = await context.Teams.FirstAsync();
+
+// Select record that meets a condition or throws error
+var teamTwo = await context.Teams.FirstAsync(t => t.Id == 2);
+// returns a record or null
+var teamTwoOrNull = await context.Teams.FirstOrDefaultAsync(t => t.Id == 2);
+//Selecting a single record - Only one record should be returned or returns error if there is more than one record that can be returned
+var teamThreeA = await context.Teams.SingleAsync(t => t.Id == 3);
+//Selecting a single record - Only one record should be returned or returns null
+var teamThreeB = await context.Teams.SingleOrDefaultAsync(t => t.Id == 3);
+// Selecting based on Id
+var teamBasedOnId = await context.Teams.FindAsync(5);
+if (teamBasedOnId != null) Console.WriteLine(teamBasedOnId.Name);
+
+// Select all teams
+void GetAllTeams()
+{
+    var teams = context.Teams.ToList();
+
+    foreach (var team in teams) Console.WriteLine(team.Name);
+}
--- EntityFrameworkCore.Data/FootballLeagueDbContext.cs
+++ EntityFrameworkCore.Data/FootballLeagueDbContext.cs
@@ -6,43 +6,54 @@ namespace EntityFrameworkCore.Data;
 
 public class FootballLeagueDbContext : DbContext
 {
+    public FootballLeagueDbContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = Path.Combine(path, "FootballLeague_EfCore.db");
+    }
+
+
     public DbSet<Team> Teams { get; set; }
     public DbSet<Coach> Coaches { get; set; }
+
+    public string DbPath { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         //SQL Server example
         //optionsBuilder.UseSqlServer(
-            //"Data Source=(localdb)\\MSSQLLocalDB; Initial Catalog=FootballLeague_EfCore; Encrypt=False");
-        
+        //"Data Source=(localdb)\\MSSQLLocalDB; Initial Catalog=FootballLeague_EfCore; Encrypt=False");
+
         //This will be moved out to a settings file. DO NOT DO THAT IN REAL LIFE.
         // Added logging of queries
-        // EnableSensitiveDataLogging and EnableDetailErrors is only for this tutorial. DO NOT DO THIS IN REAL LIFE.
-        optionsBuilder.UseSqlite($"Data Source=FootballLeague_EfCore.db").LogTo(Console.WriteLine, LogLevel.Information).EnableSensitiveDataLogging().EnableDetailedErrors();
+        // EnableSensitiveDataLogging and EnableDetailErrors is only for this tutorial. DO NOT DO THIS IN PROD.
+        optionsBuilder.UseSqlite($"Data Source={DbPath}").LogTo(Console.WriteLine, LogLevel.Information)
+            .EnableSensitiveDataLogging().EnableDetailedErrors();
     }
 
     // Creating seed data
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Team>().HasData(
-                new Team
-                {
-                    Id = 1,
-                    Name = "Nebraska Huskers",
-                    CreatedDate = DateTimeOffset.UtcNow.DateTime
-                }, 
-                new Team
-                {
-                    Id = 2,
-                    Name = "Oklahoma Sooners",
-                    CreatedDate = DateTimeOffset.UtcNow.DateTime
-                },
-                new Team
-                {
-                    Id = 3,
-                    Name = "Miami Hurricanes",
-                    CreatedDate = DateTimeOffset.UtcNow.DateTime
-                }
-            );
+            new Team
+            {
+                Id = 1,
+                Name = "Nebraska Huskers",
+                CreatedDate = DateTimeOffset.UtcNow.DateTime
+            },
+            new Team
+            {
+                Id = 2,
+                Name = "Oklahoma Sooners",
+                CreatedDate = DateTimeOffset.UtcNow.DateTime
+            },
+            new Team
+            {
+                Id = 3,
+                Name = "Miami Hurricanes",
+                CreatedDate = DateTimeOffset.UtcNow.DateTime
+            }
+        );
     }
 }
