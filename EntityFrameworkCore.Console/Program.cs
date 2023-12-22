14f47f47414956a741ed25459b790ab5f67002ce
rkeenan <ross.keenan@protonmail.comm>
2023-12-21 19:25:53 -0600

    Extracted queries out to their own method.

--- EntityFrameworkCore.Console/Program.cs
+++ EntityFrameworkCore.Console/Program.cs
@@ -5,27 +5,38 @@ using Microsoft.EntityFrameworkCore;
 using var context = new FootballLeagueDbContext();
 
 
-//GetAllTeams();
+await GetAllTeams();
+await GetOneTeam();
 
-// Getting the first one in the list
-var teamOne = await context.Teams.FirstAsync();
-
-// Select record that meets a condition or throws error
-var teamTwo = await context.Teams.FirstAsync(t => t.Id == 2);
-// returns a record or null
-var teamTwoOrNull = await context.Teams.FirstOrDefaultAsync(t => t.Id == 2);
-//Selecting a single record - Only one record should be returned or returns error if there is more than one record that can be returned
-var teamThreeA = await context.Teams.SingleAsync(t => t.Id == 3);
-//Selecting a single record - Only one record should be returned or returns null
-var teamThreeB = await context.Teams.SingleOrDefaultAsync(t => t.Id == 3);
-// Selecting based on Id
-var teamBasedOnId = await context.Teams.FindAsync(5);
-if (teamBasedOnId != null) Console.WriteLine(teamBasedOnId.Name);
 
 // Select all teams
-void GetAllTeams()
+async Task GetAllTeams()
 {
-    var teams = context.Teams.ToList();
+    var teams = await context.Teams.ToListAsync();
 
     foreach (var team in teams) Console.WriteLine(team.Name);
+}
+
+// Get one team
+async Task GetOneTeam()
+{
+    // Getting the first one in the list
+    var teamOne = await context.Teams.FirstAsync();
+    if (teamOne != null) Console.WriteLine(teamOne.Name);
+
+    // Select record that meets a condition or throws error
+    var teamTwo = await context.Teams.FirstAsync(t => t.Id == 2);
+    if (teamTwo != null) Console.WriteLine(teamTwo.Name);
+// returns a record or null
+    var teamTwoOrNull = await context.Teams.FirstOrDefaultAsync(t => t.Id == 2);
+    if (teamTwoOrNull != null) Console.WriteLine(teamTwoOrNull.Name);
+    //Selecting a single record - Only one record should be returned or returns error if there is more than one record that can be returned
+    var teamThreeA = await context.Teams.SingleAsync(t => t.Id == 3);
+    if (teamThreeA != null) Console.WriteLine(teamThreeA.Name);
+    //Selecting a single record - Only one record should be returned or returns null
+    var teamThreeB = await context.Teams.SingleOrDefaultAsync(t => t.Id == 3);
+    if (teamThreeB != null) Console.WriteLine(teamThreeB);
+// Selecting based on Id
+    var teamBasedOnId = await context.Teams.FindAsync(5);
+    if (teamBasedOnId != null) Console.WriteLine(teamBasedOnId.Name);
 }
