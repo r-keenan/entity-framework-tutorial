d5f1fa6123c0fd8b6e2df3dbab26d5e508b30cfe
rkeenan <ross.keenan@protonmail.comm>
2023-12-25 08:28:13 -0600

    Committing migrations.

---
+++ EntityFrameworkCore.Data/Migrations/20231225140510_AddedCoachLeague.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EntityFrameworkCore.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedCoachLeague : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CoachId",
+                table: "Teams",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreateBy",
+                table: "Teams",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "LeagueId",
+                table: "Teams",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ModifiedBy",
+                table: "Teams",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "Teams",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreateBy",
+                table: "Coaches",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ModifiedBy",
+                table: "Coaches",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "Coaches",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.CreateTable(
+                name: "Leagues",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CreatedDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    CreateBy = table.Column<string>(type: "TEXT", nullable: true),
+                    ModifiedDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    ModifiedBy = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Leagues", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Matches",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    HomeTeamId = table.Column<int>(type: "INTEGER", nullable: false),
+                    AwayTeamId = table.Column<int>(type: "INTEGER", nullable: false),
+                    TicketPrice = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Date = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    CreateBy = table.Column<string>(type: "TEXT", nullable: true),
+                    ModifiedDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    ModifiedBy = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Matches", x => x.Id);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CoachId", "CreateBy", "CreatedDate", "LeagueId", "ModifiedBy", "ModifiedDate" },
+                values: new object[] { 0, null, new DateTime(2023, 12, 25, 14, 5, 10, 390, DateTimeKind.Unspecified).AddTicks(7210), 0, null, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CoachId", "CreateBy", "CreatedDate", "LeagueId", "ModifiedBy", "ModifiedDate" },
+                values: new object[] { 0, null, new DateTime(2023, 12, 25, 14, 5, 10, 390, DateTimeKind.Unspecified).AddTicks(7210), 0, null, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "CoachId", "CreateBy", "CreatedDate", "LeagueId", "ModifiedBy", "ModifiedDate" },
+                values: new object[] { 0, null, new DateTime(2023, 12, 25, 14, 5, 10, 390, DateTimeKind.Unspecified).AddTicks(7210), 0, null, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Leagues");
+
+            migrationBuilder.DropTable(
+                name: "Matches");
+
+            migrationBuilder.DropColumn(
+                name: "CoachId",
+                table: "Teams");
+
+            migrationBuilder.DropColumn(
+                name: "CreateBy",
+                table: "Teams");
+
+            migrationBuilder.DropColumn(
+                name: "LeagueId",
+                table: "Teams");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedBy",
+                table: "Teams");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "Teams");
+
+            migrationBuilder.DropColumn(
+                name: "CreateBy",
+                table: "Coaches");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedBy",
+                table: "Coaches");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "Coaches");
+
+            migrationBuilder.UpdateData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2023, 12, 21, 2, 5, 14, 510, DateTimeKind.Unspecified).AddTicks(8910));
+
+            migrationBuilder.UpdateData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2023, 12, 21, 2, 5, 14, 510, DateTimeKind.Unspecified).AddTicks(8920));
+
+            migrationBuilder.UpdateData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2023, 12, 21, 2, 5, 14, 510, DateTimeKind.Unspecified).AddTicks(8920));
+        }
+    }
+}
